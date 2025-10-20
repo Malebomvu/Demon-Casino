fba86931de98cee2dcb1e30de526dc951b781c0f
Malebomvu <2671127@students.wits.ac.za>
2025-10-20 23:33:02 +0200

    Revert "timeline and volume fix"

--- My project/Assets/Scripts/SettingsScript.cs
+++ My project/Assets/Scripts/SettingsScript.cs
@@ -1,25 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.UI;  // Needed to reference the slider
 
 public class SettingsScript : MonoBehaviour
 {
     public AudioMixer mixer;
-    public Slider volumeSlider;
-
-    private const string VolumePrefKey = "masterVolume";
-
-    void Start()
-    {
-        // Load saved volume (or default to 0 dB if none is saved)
-        float savedVolume = PlayerPrefs.GetFloat(VolumePrefKey, 0f);
-        mixer.SetFloat("volume", savedVolume);
-        volumeSlider.value = savedVolume;
-    }
-
     public void SetVolume(float volume)
     {
         mixer.SetFloat("volume", volume);
-        PlayerPrefs.SetFloat(VolumePrefKey, volume);
     }
 }
