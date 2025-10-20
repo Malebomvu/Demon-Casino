29fd27964d9e829031d059ad92cd3f27b5a4c16a
REMARUTEMPEST <2671127@students.wits.ac.za>
2025-10-20 20:24:35 +0200

    Asset Intergration
    I added anither asset int unity.

---
+++ My project/Assets/Scripts/Enemydamage1.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Enemydamage1 : MonoBehaviour
+{
+    public float damage = 10f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided with" + collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Demons"))
+        {
+            Debug.Log("Demon was hit");
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
