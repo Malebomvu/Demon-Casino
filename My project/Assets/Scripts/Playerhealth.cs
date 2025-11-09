687c113361f44c93d4ae5929b9ba0105b33795af
REMARUTEMPEST <2671127@students.wits.ac.za>
2025-11-09 12:58:57 +0200

    Pulled Changes
    I just finished pulling changes from the main branch.

--- My project/Assets/Scripts/Playerhealth.cs
+++ My project/Assets/Scripts/Playerhealth.cs
@@ -1,26 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class Playerhealth : MonoBehaviour
+public class PlayerHealth : MonoBehaviour
 {
-    public int health;
-    public int maxHealth = 50;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject hud;
+    public GameObject inv;
+    public GameObject deathScreen;
+    public GameObject player;
+    public Slider healthSlider;
+
+    public float health = 100f;
+    public float maxHealth = 100f;
+
+    public TMP_Text damagePopupText;
+
     void Start()
     {
-        health = maxHealth;
+        deathScreen.SetActive(false);
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = health;
+        damagePopupText.gameObject.SetActive(false);
     }
-    public void TakeDamage(int amount)
-    {
-        health -= amount;
-        if (health <= 0)
-        {
-            Destroy(gameObject);
-        }
-    }    
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (health <= 0)
+        {
+            health = 0;
+            player.GetComponent<FirstPersonController>().enabled = false;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            hud.SetActive(false);
+            inv.SetActive(false);
+            deathScreen.SetActive(true);
+        }
+
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+
+        healthSlider.value = health;
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+        ShowDamagePopup("-" + damageAmount + " HP");
+        healthSlider.value = health;
+    }
+
+    private void ShowDamagePopup(string message)
+    {
+        damagePopupText.text = message;
+        damagePopupText.gameObject.SetActive(true);
+        Invoke(nameof(HideDamagePopup), 2f);
+    }
+
+    private void HideDamagePopup()
+    {
+        damagePopupText.gameObject.SetActive(false);
+    }
+
+    public void Heal(float healAmount)
+    {
+        health += healAmount;
+        if (health > maxHealth)
+            health = maxHealth;
+
+        healthSlider.value = health;
     }
 }
